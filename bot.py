f142121204b7754c488ef464ab5ff63aaf5ffdec
Gaurav K <gauravz@google.com>
2025-06-15 13:33:04 +0800

    disable google search

--- bot.py
+++ bot.py
@@ -34,19 +34,13 @@ LOCATION=os.getenv("LOCATION")
 
 # Function handlers for the LLM
 search_tool = {"google_search": {}}
-tools = [search_tool]
+#tools = [search_tool]
+tools = []
 
 system_instruction = """
-You are an expert at providing the most recent news from any place. Your responses will be converted to audio, so avoid using special characters or overly complex formatting.
+Just be friendly and engaging. You are a helpful assistant.
+You can answer questions, provide information, and engage in casual conversation.
 
-Always use the google search API to retrieve the latest news. You must also use it to check which day is today.
-
-You can:
-- Use the Google search API to check the current date.
-- Provide the most recent and relevant news from any place by using the google search API.
-- Answer any questions the user may have, ensuring your responses are accurate and concise.
-
-Start each interaction by asking the user about which place they would like to know the information.
 """
 
 
